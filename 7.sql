95728a514363717d91667b75349ed295e7ae58a0
SuhanovvMax <115794988+SuhanovvMax@users.noreply.github.com>
2024-06-30 23:28:25 +0300

    Create 7.sql

---
+++ 7.sql
@@ -0,0 +1,66 @@
+drop table if exists stage.shk_ext_log;
+create table stage.shk_ext_log
+(
+    shk_id Int64,
+    dt     DateTime,
+    ext_barcode String
+)
+engine = MergeTree() order by shk_id;
+
+drop table if exists buf_.shk_ext_log;
+create table buf_.shk_ext_log
+(
+    shk_id Int64,
+    dt     DateTime,
+    ext_barcode String
+)
+engine = Buffer('stage', 'shk_ext_log', 16, 10, 100, 10000, 1000000, 1000000, 100000000)
+
+insert into buf_.shk_ext_log (shk_id, dt, ext_barcode) values
+(13526441, '2024-05-30 23:04:50', 'efsgjoisjg3213fsdadas1'),
+(1352644211, '2024-05-30 23:04:50', 'efsgjoisjg3213dsada1'),
+(1352644221, '2024-05-30 23:04:50', 'efsgjoisjg3213vdfgd1'),
+(135264424321, '2024-05-30 23:04:50', 'efsgjoisjg321fdsadas31'),
+(135264423211, '2024-05-30 23:04:50', 'efsgjoisjg32133212131'),
+(13526442351, '2024-05-30 23:04:50', 'efsgjoisjg32133211'),
+(13526442321, '2024-05-30 23:04:50', 'efsgjoisjg3213fdsgs1'),
+(13526442211, '2024-05-30 23:04:50', 'efsgjoisjg3213dsa1'),
+(135264432121, '2024-05-30 23:04:50', 'efsgjoisefsdfdasjg32131'),
+(1352647421, '2024-05-30 23:04:50', 'efsgjoisjg32dsa131'),
+(1352664421, '2024-05-30 23:04:50', 'efsgjoisjg321dsada31'),
+(1352564421, '2024-05-30 23:04:50', 'efsgjoisjg321gdsf31'),
+(1352564421, '2024-05-30 23:22:09', 'efsgjoisjg321gdsf31'),
+(1354264421, '2024-05-30 23:04:50', 'efsgjoisjg3213dsa1');
+
+select *
+from stage.shk_ext_log;
+
+create table currents.shk_ext_cur
+(
+    shk_id      Int64,
+    dt          DateTime,
+    ext_barcode String
+)
+engine = ReplacingMergeTree(dt)
+order by shk_id;
+
+drop view if exists stage.shk_ext_log_cur;
+create materialized view stage.shk_ext_log_cur to currents.shk_ext_cur as
+    select shk_id,
+           dt,
+           ext_barcode
+    from stage.shk_ext_log;
+
+
+
+show view stage.shk_ext_log_cur;
+
+
+select *
+from buf_.shk_ext_log;
+
+select *
+from stage.shk_ext_log;
+
+select *
+from currents.shk_ext_cur;
